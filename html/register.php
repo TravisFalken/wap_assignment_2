f1c546a5e333e36be34f3024f1f373872447fd69
TravisFalken <travis.falkenberg141@gmail.com>
2021-06-06 00:57:08 +1200

    Updated register to better naming

--- html/register.php
+++ html/register.php
@@ -6,32 +6,32 @@ include('includes/header.html');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 
-	// Need the database connection:
+	//Get db connection:
 	require(MYSQL);
 
 	// Trim all the incoming data:
 	$trimmed = array_map('trim', $_POST);
 
 	// Assume invalid values:
-	$fn = $ln = $e = $p = FALSE;
+	$firstName = $lastName = $email = $pass = FALSE;
 
 	// Check for a first name:
 	if (preg_match('/^[A-Z \'.-]{2,20}$/i', $trimmed['first_name'])) {
-		$fn = mysqli_real_escape_string($dbc, $trimmed['first_name']);
+		$firstName = mysqli_real_escape_string($dbc, $trimmed['first_name']);
 	} else {
 		echo '<p class="error">Please enter your first name!</p>';
 	}
 
 	// Check for a last name:
 	if (preg_match('/^[A-Z \'.-]{2,40}$/i', $trimmed['last_name'])) {
-		$ln = mysqli_real_escape_string($dbc, $trimmed['last_name']);
+		$lastName = mysqli_real_escape_string($dbc, $trimmed['last_name']);
 	} else {
 		echo '<p class="error">Please enter your last name!</p>';
 	}
 
 	// Check for an email address:
 	if (filter_var($trimmed['email'], FILTER_VALIDATE_EMAIL)) {
-		$e = mysqli_real_escape_string($dbc, $trimmed['email']);
+		$email = mysqli_real_escape_string($dbc, $trimmed['email']);
 	} else {
 		echo '<p class="error">Please enter a valid email address!</p>';
 	}
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 	// Check for a password and match against the confirmed password:
 	if (strlen($trimmed['password1']) >= 10) {
 		if ($trimmed['password1'] == $trimmed['password2']) {
-			$p = password_hash($trimmed['password1'], PASSWORD_DEFAULT);
+			$pass = password_hash($trimmed['password1'], PASSWORD_DEFAULT);
 		} else {
 			echo '<p class="error">Your password did not match the confirmed password!</p>';
 		}
@@ -47,26 +47,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 		echo '<p class="error">Please enter a valid password!</p>';
 	}
 
-	if ($fn && $ln && $e && $p) { // If everything's OK...
+	if ($firstName && $lastName && $email && $pass) { // If everything's OK...
 
 		// Make sure the email address is available:
-		$q = "SELECT user_id FROM users WHERE email='$e'";
-		$r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+		$q = "SELECT user_id FROM users WHERE email='$email'";
+		$res = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
-		if (mysqli_num_rows($r) == 0) { // Available.
+		if (mysqli_num_rows($res) == 0) { // Available.
 
 			// Create the activation code:
-			$a = md5(uniqid(rand(), true));
+			$activationCode = md5(uniqid(rand(), true));
 
 			// Add the user to the database:
-			$q = "INSERT INTO users (email, pass, first_name, last_name, active, registration_date) VALUES ('$e', '$p', '$fn', '$ln', '$a', NOW() )";
-			$r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+			$q = "INSERT INTO users (email, pass, first_name, last_name, active, registration_date) VALUES ('$email', '$pass', '$firstName', '$ln', '$activationCode', NOW() )";
+			$res = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
-			if (mysqli_affected_rows($dbc) == 1) { // If it ran OK.
+			if (mysqli_affected_rows($dbc) == 1) { //Make sure user has been added
 
 				// Send the email:
-				$body = "Thank you for registering at <whatever site>. To activate your account, please click on this link:\n\n";
-				$body .= BASE_URL . 'activate.php?x=' . urlencode($e) . "&y=$a";
+				$body = "Thank you for registering at computersRUs. To activate your account, please click on this link:\n\n";
+				$body .= BASE_URL . 'activate.php?x=' . urlencode($email) . "&y=$activationCode";
 				mail($trimmed['email'], 'Registration Confirmation', $body, 'From: admin@sitename.com');
 
 				// Finish the page:
@@ -77,17 +77,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 			} else { // If it did not run OK.
 				echo '<p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>';
 			}
-
 		} else { // The email address is not available.
 			echo '<p class="error">That email address has already been registered. If you have forgotten your password, use the link at right to have your password sent to you.</p>';
 		}
-
 	} else { // If one of the data tests failed.
 		echo '<p class="error">Please try again.</p>';
 	}
 
 	mysqli_close($dbc);
-
 } // End of the main Submit conditional.
 ?>
 
@@ -95,15 +92,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 <form action="register.php" method="post">
 	<fieldset>
 
-	<p><strong>First Name:</strong> <input type="text" name="first_name" size="20" maxlength="20" value="<?php if (isset($trimmed['first_name'])) echo $trimmed['first_name']; ?>"></p>
+		<p><strong>First Name:</strong> <input type="text" name="first_name" size="20" maxlength="20" value="<?php if (isset($trimmed['first_name'])) echo $trimmed['first_name']; ?>"></p>
 
-	<p><strong>Last Name:</strong> <input type="text" name="last_name" size="20" maxlength="40" value="<?php if (isset($trimmed['last_name'])) echo $trimmed['last_name']; ?>"></p>
+		<p><strong>Last Name:</strong> <input type="text" name="last_name" size="20" maxlength="40" value="<?php if (isset($trimmed['last_name'])) echo $trimmed['last_name']; ?>"></p>
 
-	<p><strong>Email Address:</strong> <input type="email" name="email" size="30" maxlength="60" value="<?php if (isset($trimmed['email'])) echo $trimmed['email']; ?>"> </p>
+		<p><strong>Email Address:</strong> <input type="email" name="email" size="30" maxlength="60" value="<?php if (isset($trimmed['email'])) echo $trimmed['email']; ?>"> </p>
 
-	<p><strong>Password:</strong> <input type="password" name="password1" size="20" value="<?php if (isset($trimmed['password1'])) echo $trimmed['password1']; ?>"> <small>At least 10 characters long.</small></p>
+		<p><strong>Password:</strong> <input type="password" name="password1" size="20" value="<?php if (isset($trimmed['password1'])) echo $trimmed['password1']; ?>"> <small>At least 10 characters long.</small></p>
 
-	<p><strong>Confirm Password:</strong> <input type="password" name="password2" size="20" value="<?php if (isset($trimmed['password2'])) echo $trimmed['password2']; ?>"></p>
+		<p><strong>Confirm Password:</strong> <input type="password" name="password2" size="20" value="<?php if (isset($trimmed['password2'])) echo $trimmed['password2']; ?>"></p>
 	</fieldset>
 
 	<div align="center"><input type="submit" name="submit" value="Register"></div>

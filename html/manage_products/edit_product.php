58b3c37013d837a2a0dd01f06940e4086d3364f9
TravisFalken <travis.falkenberg141@gmail.com>
2021-06-17 03:26:03 +1200

    Fixed bugs in edit_product

--- html/manage_products/edit_product.php
+++ html/manage_products/edit_product.php
@@ -26,9 +26,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $trimmed = array_map('trim', $_POST);
 
 
-    $productTitle = $productDescription = $productPrice = $productSKU = $productQty = $profilePhoto = $profilePhotoUploadSuccess = $dbProductPhotoInsert = $dbPhotoInsert =  $profilePhotoNotUploaded = FALSE;
+    $productTitle = $productDescription = $productPrice = $productSKU = $productQty = $productID = $profilePhoto = $profilePhotoUploadSuccess = $dbProductPhotoInsert = $dbPhotoInsert =  $profilePhotoNotUploaded = FALSE;
+    $transactionSuccess = true;
 
-    if (isset($_FILES['profilePhoto']) && file_exists($_FILES['profilePhoto']['tmp_name']) && in_array($_FILES['profilePhoto']['type'], $allowedFiles)) {
+    if (isset($_FILES['profilePhoto']['tmp_name']) && file_exists($_FILES['profilePhoto']['tmp_name']) && in_array($_FILES['profilePhoto']['type'], $allowedFiles)) {
 
         $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
 
@@ -38,9 +39,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // Close the resource:
         finfo_close($fileinfo);
-    } else if (!isset($_FILES['profilePhoto'])) {
+    } else {
         $profilePhotoNotUploaded = true;
     }
+
+    echo $_FILES['profilePhoto']['tmp_name'];
 
     //Get the product title
     if (isset($trimmed['productTitle'])) {
@@ -77,8 +80,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo '<p>Please enter the product Qty';
     }
 
+    //Get product Id
+    if (isset($trimmed['productId']) && filter_var($trimmed['productId'], FILTER_VALIDATE_INT, array('min-range' => 1))) {
+        $productID =  $trimmed['productId'];
+    }
+
+    echo 'test: ' . $profilePhotoNotUploaded;
     //Make sure everything is okay
-    if ($productTitle && $productDescription && $productPrice && $productSKU && $productQty && ($profilePhoto || $profilePhotoNotUploaded)) {
+    if ($productID && $productTitle && $productDescription && $productPrice && $productSKU && $productQty && ($profilePhoto || $profilePhotoNotUploaded)) {
         $createdUserID = $_SESSION['user_id'];
         if ($profilePhoto) {
             $newFileName = uniqid(rand(), true);
@@ -90,66 +99,138 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         //Begin Transaction
         mysqli_begin_transaction($dbc);
 
-        $q = "  UPDATE products
+        //Make sure new values are being added
+        $q = "  SELECT
+                    product_id
+                FROM products
+                WHERE   product_id              = $productID
+                AND     (product_title           <> '$productTitle'
+                            OR      product_description     <> '$productDescription'
+                            OR      price                   <> $productPrice
+                            OR      stock_quantity          <> $productQty
+                            OR      sku                     <> '$productSKU'
+                        )
+                ";
+
+        $r = mysqli_query($dbc, $q);
+        echo 'Rows:' . mysqli_num_rows($r);
+        if (mysqli_num_rows($r) > 0) {
+
+            $q = "  UPDATE products
                 SET 
-                    product_title       = $productTitle
-                ,   product_description = $productDescription
+                    product_title       = '$productTitle'
+                ,   product_description = '$productDescription'
                 ,   price               = $productPrice
                 ,   stock_quantity      = $productQty
-                ,   sku                 = $productSKU
-                WHERE product_id = $id";
+                ,   sku                 = '$productSKU'
+                WHERE product_id = $productID";
 
-        $res = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+            $res = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
+            if (mysqli_affected_rows($dbc) == 1) {
+                $transactionSuccess = false;
+            }
+        }
         //Make sure row has been added
-        echo 'First:' . mysqli_affected_rows($dbc) . '<br>';
-        if (mysqli_affected_rows($dbc) == 1) {
-            $InsertedProductId = mysqli_insert_id($dbc);
-            //echo $InsertedProductId;
+        if ($transactionSuccess) {
+            if ($profilePhoto) {
+                $profilePhotoSize = $profilePhoto['size'];
+                $profilePhotoImageType = $profilePhoto['type'];
 
-            $profilePhotoSize = $profilePhoto['size'];
-            $profilePhotoImageType = $profilePhoto['type'];
-            $q = "INSERT INTO images (image_title, image_size, image_type, image_name, created_by_user_id)
+                //If there is an existing photo delete it
+                $q = "  SELECT
+                                product_image_id
+                            ,   ig.image_id
+                            ,   ig.image_name
+                        FROM product_images pi
+                        INNER JOIN  images  ig ON ig.image_id = pi.image_id
+                        WHERE   pi.product_id   = $productID
+                        AND     pi.cover_image  = 1
+                        LIMIT 1";
+
+                $r = mysqli_query($dbc, $q);
+
+                //Only delete if image exists
+                if (mysqli_num_rows($r) == 1) {
+                    $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+                    $imageID = $row['image_id'];
+                    $productImageID = $row['product_image_id'];
+                    $oldImageName = $row['image_name'];
+
+                    //Delete from product images table
+                    $q = " DELETE FROM product_images WHERE product_image_id = $productImageID";
+
+                    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
+                    if (mysqli_affected_rows($dbc) != 1) {
+                        $transactionSuccess = false;
+                    }
+
+                    //Delete from images table
+                    $q = "DELETE FROM images WHERE image_id = $imageID";
+
+                    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
+                    if (mysqli_affected_rows($dbc) != 1) {
+                        $transactionSuccess = false;
+                    }
+                }
+
+
+                //Insert into image table
+                $q = "INSERT INTO images (image_title, image_size, image_type, image_name, created_by_user_id)
                     VALUES ('Profile Photo', $profilePhotoSize, '$profilePhotoImageType', '$finalProfilePhotoName', $createdUserID)";
 
-            $res = mysqli_query($dbc, $q);
-            echo 'Second:' . mysqli_affected_rows($dbc);
-            if (mysqli_affected_rows($dbc) == 1) {
-                $dbPhotoInsert = true;
-            }
+                $res = mysqli_query($dbc, $q);
+                if (mysqli_affected_rows($dbc) != 1) {
+                    $transactionSuccess = false;
+                }
 
-            $insertedImageID = mysqli_insert_id($dbc);
+                $insertedImageID = mysqli_insert_id($dbc);
+                //Insert into the join table product images
+                $q = "INSERT INTO product_images (product_id, image_id, cover_image, created_by_user_id)
+                    VALUES($productID, $insertedImageID, 1, $createdUserID)";
 
-            $q = "INSERT INTO product_images (product_id, image_id, cover_image, created_by_user_id)
-                    VALUES($InsertedProductId, $insertedImageID, 1, $createdUserID)";
+                $res = mysqli_query($dbc, $q);
+                if (mysqli_affected_rows($dbc) != 1) {
+                    $transactionSuccess = false;
+                }
 
-            $res = mysqli_query($dbc, $q);
-            echo 'Third:' . mysqli_affected_rows($dbc);
-            if (mysqli_affected_rows($dbc) == 1) {
-                $dbProductPhotoInsert = true;
-            }
-            if ($dbPhotoInsert && $dbPhotoInsert) {
-                if (move_uploaded_file($profilePhoto['tmp_name'], "../uploads/{$profilePhoto['name']}")) {
-                    $profilePhotoSuccess = true;
+                // Add new Image
+                if ($transactionSuccess) {
+                    if (!move_uploaded_file($profilePhoto['tmp_name'], "../uploads/{$profilePhoto['name']}")) {
+                        $transactionSuccess = false;
+                    }
+                }
+                //Remove old image
+                if (isset($oldImageName) && $transactionSuccess) {
+                    $filePath = '../uploads/' . $oldImageName;
+                    if (!unlink($filePath)) {
+                        $transactionSuccess = false;
+                    }
                 }
             }
-
-            if ($dbPhotoInsert && $profilePhotoSuccess && $dbProductPhotoInsert) {
+            if ($transactionSuccess) {
                 mysqli_commit($dbc);
-                //REDIRECT the user because they do not have permission
-                $url = BASE_URL . 'index.php'; // Define the URL.
+                //REDIRECT the user
+                mysqli_close($dbc);
+                $url = BASE_URL . 'manage_products/manage_product.php'; // Define the URL.
                 ob_end_clean(); // Delete the buffer.
                 header("Location: $url");
                 exit(); // Quit the script.
+            } else {
+                mysqli_rollback($dbc);
+                echo '<p class="error">You could not be edit the product due to a system error. We apologize for any inconvenience.</p>';
             }
 
             mysqli_rollback($dbc);
         } else {
             mysqli_rollback($dbc);
-            echo '<p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>';
+            echo '<p class="error">You could not be edit the product due to a system error. We apologize for any inconvenience.</p>';
         }
     }
     mysqli_close($dbc);
+    echo '<p class="error">You could not be edit the product due to a system error. We apologize for any inconvenience.</p>';
 } else {
     //Connect to database
     require('../../mysqli_connect.php');
@@ -190,12 +271,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo '  <div class="container">
                     <h1>Edit Product</h1>
                 </div>
-                <form enctype="multipart/form-data" action="add_product.php" method="post">
+                <form enctype="multipart/form-data" action="edit_product.php" method="post">
                     <div class="container">
                         <div class="row d-flex justify-content-center">
-                            <div class="col-6-md d-flex custom-view-product-image-div">
+                            <div class="col-8-md d-flex custom-view-product-image-div">
                                 <img class="custom-view-product-image" src="' . BASE_URL . 'uploads/' . $row['cover_image_name'] . '">
                             </div>
+                 
+                        </div>
+                        <div class="row d-flex justify-content-center">
+                        <div class="col-6-md">
+                        ';
+        echo (isset($row['cover_image_name'])) ?  '<a class="btn btn-danger btn-lg" href ="delete_photo.php">Remove Photo</a>' : '';
+        echo                '</div>
                         </div>
                         <div class="form-group">
                             <label for="profilePhoto">Select Profile Photo:</label>
@@ -221,6 +309,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             <label for="password2">Product Quantity:</label>
                             <input type="number" class="form-control" name="productQty" size="20" value="' . $row['stock_quantity'] . '">
                         </div>
+                        <input type="hidden" name="productId" value="' . $row["product_id"] . '">
                         <div class="form-group row">
                             <div class="col-sm-6">
                                 <button type="submit" class="btn btn-success btn-block"><i class="far fa-save"></i>  Save</button>
